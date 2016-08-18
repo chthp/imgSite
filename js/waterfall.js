6fca51edc5385f911cd891c418f94cb17a749ffc
lzl852 <lzl852@qq.com>
2016-08-18 14:44:49 +0800

    去掉不需要的

--- js/waterfall.js
+++ js/waterfall.js
@@ -7,7 +7,7 @@
     $container.imagesLoaded( function(){
         $container.masonry({
             itemSelector : '.cell',
-            isAnimated: true,
+            isAnimated: false,
             isResizableL:true
         });
 
